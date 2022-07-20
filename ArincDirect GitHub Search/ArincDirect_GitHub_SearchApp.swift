de9a8efe0bb28521f3548ff874da07b416b1bf44
Jordan Kauffman <jdkauffman11@gmail.com>
2022-07-19 20:49:23 -0500

    Initial Commit

---
+++ ArincDirect GitHub Search/ArincDirect_GitHub_SearchApp.swift
@@ -0,0 +1,17 @@
+//
+//  ArincDirect_GitHub_SearchApp.swift
+//  ArincDirect GitHub Search
+//
+//  Created by Jordan Kauffman on 7/19/22.
+//
+
+import SwiftUI
+
+@main
+struct ArincDirect_GitHub_SearchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ArincDirect GitHub Search/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ArincDirect GitHub Search
+//
+//  Created by Jordan Kauffman on 7/19/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
